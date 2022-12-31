e05b1216020fe142caf91a1d1e1d904416d10997
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2022-12-31 22:14:33 +0530

    Prob2.java

---
+++ Prob2.java
@@ -0,0 +1,11 @@
+package Assign2;
+
+import java.util.Scanner;
+public class Prob2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String lin1 = sc.next();
+        String lin2 = sc.next();
+        System.out.println(lin1+lin2);
+    }
+}
